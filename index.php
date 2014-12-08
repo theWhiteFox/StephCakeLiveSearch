22635ed6a05c151b0541780a014c5567e0e96441
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-08 18:57:42 +0000

    A basic search app using AJAX and JSON

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<!-- @ Stephen O'Connor -->
 <html>
    <head>
+      <title>Live Search</title>
       <meta charset="UTF-8">
-      <title></title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">      
+      <link href="mystyle.css" rel="stylesheet"/>
    </head>
-   <body>
-      <?php
-      // put your code here
-      ?>
+   <body >  
+      <div id="searcharea"> 
+         <label for="search">This is a Live Search</label>
+         <p>Enter the name or info about a popular character from Boardwalk Empire</p>
+         <input type="search" name="search" id="search" placeholder="name or info"/>      
+      </div>
+      <div id='update'></div> 
+      <script src="jquery.js"></script>      
+      <script src="script.js"></script>
    </body>
 </html>
